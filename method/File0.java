a3c433fa1a66cb2bbbfea3e966864285eea1adb3
manju2518 <m.p.manju2006@gmail.com>
2025-03-12 14:26:37 +0530

    This is the method program

---
+++ method/File0.java
@@ -0,0 +1,10 @@
+public class File0{
+    public void multiply(int num1,int num2){
+    int num=num1*num2;
+    System.out.println(num);
+}
+public static void main(String args[]){
+    File0 obj=new File0();
+    obj.File0(40*10)
+}
+}
