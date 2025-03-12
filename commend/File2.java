e9785e18ebaeeb719621e82ac1c6a31b4d6b7a3c
manju2518 <m.p.manju2006@gmail.com>
2025-03-12 14:18:52 +0530

    I am complete second command

--- commend/File2.java
+++ commend/File2.java
@@ -1,10 +1,10 @@
 public class File2{
     /*
-
-
+      1)demo file
+      2)using variables
     */
 public static void main(String args[]){
-   int num=10;//this is int value
+   int num=10;//this is integer value
    System.out.println(num);
     }
 }
