2de6ef962607cb5571c1a73547b501d060d12679
manju2518 <m.p.manju2006@gmail.com>
2025-03-12 14:07:13 +0530

    I am here

--- variable/Example.java
+++ variable/Example.java
@@ -1,10 +1,6 @@
 public class Example{
-    public void multiply(int num1,int num2){
-    int num=num1*num2;
-    System.out.println(num);
-    }
-    public static void main(String args[]){
-    Example obj=new Example();
-    obj.multiply(5,10);
+public static void main(String args[]){
+   int num=10;
+   System.out.println(num);
     }
 }
