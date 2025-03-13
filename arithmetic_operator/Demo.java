55af4f34483798f151a5ec252c8cb9d624b73219
manju2518 <m.p.manju2006@gmail.com>
2025-03-13 10:22:41 +0530

    Arithmetic Operator

---
+++ arithmetic_operator/Demo.java
@@ -0,0 +1,9 @@
+public class Demo{ 
+    public static void main(String args[]){
+        System.out.println(30+45);
+        System.out.println(30-23);
+        System.out.println(23*4);
+        System.out.println(30/2);
+        System.out.println(35/2);
+    }
+}
