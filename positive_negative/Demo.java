7a8b49c1136069a106edd819639428f0eb3214a9
manju2518 <m.p.manju2006@gmail.com>
2025-03-13 11:13:28 +0530

    positive or negative program

---
+++ positive_negative/Demo.java
@@ -0,0 +1,15 @@
+public class Demo
+{
+    public static void main(String args[])
+    {
+        int num=-2;
+        if(num>0)
+        {
+            System.out.println("It is a positive value");
+        }
+        else
+        {
+            System.out.println("It is a negative value");
+        }
+    }
+}
