255a2321ec16e344308b52c3afd82bc72a67be37
manju2518 <m.p.manju2006@gmail.com>
2025-03-13 10:58:50 +0530

    This is else_if program

---
+++ if_else/Demo.java
@@ -0,0 +1,10 @@
+public class Demo{
+    public static void main(String args[]){
+        if(false){
+            System.out.println("It is true value");
+        }
+        else{
+            System.out.println("It is false value");
+        }
+    }
+}
