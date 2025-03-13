a90616c9fa76986361cfdb4766dbd41843161573
manju2518 <m.p.manju2006@gmail.com>
2025-03-13 15:19:20 +0530

    Addition program

---
+++ Scanner_add/Demo.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Demo{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        System.out.print("num1:");
+        int x=sc.nextInt();
+        System.out.println("num2:");
+        int y=sc.nextInt();
+        int z=x+y;
+        System.out.println("The sum is;"+z);
+    }
+}
