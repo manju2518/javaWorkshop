eeac8d47fb76fc3bd0ab40b33336d53f1d1bc1a1
manju2518 <m.p.manju2006@gmail.com>
2025-03-13 14:14:39 +0530

    Odd number program

---
+++ odd_number/demo.java
@@ -0,0 +1,11 @@
+public class Demo
+{
+    public static void main(String args[])
+    {
+        for(int i=27;i<=36;i++)
+        {
+            if(i%2!=0)
+            System.out.println(i);
+        }
+    }
+}
