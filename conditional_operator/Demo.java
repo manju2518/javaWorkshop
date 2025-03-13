0bcdc0556abe844284ec337110d636b47f309d3a
manju2518 <m.p.manju2006@gmail.com>
2025-03-13 10:31:56 +0530

    Conditional operator program

---
+++ conditional_operator/Demo.java
@@ -0,0 +1,10 @@
+public class Demo{ 
+    public static void main(String args[]){
+        System.out.println(38<3);
+        System.out.println(37>4);
+        System.out.println(34<=3);
+        System.out.println(87<=4);
+        System.out.println(45!=90);
+        System.out.println(45==45);
+    }
+}
