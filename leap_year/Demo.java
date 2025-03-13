42ce632ba028d8804b2ac7d439d29e8ed77ae216
manju2518 <m.p.manju2006@gmail.com>
2025-03-13 14:22:56 +0530

    Leap year program

---
+++ leap_year/Demo.java
@@ -0,0 +1,15 @@
+public class Demo
+{
+    public static void main(String args[])
+    {
+        int i=2020;
+        if(i%4==0)
+        {
+            System.out.println("This year is a Leap year");
+        }
+        else
+        {
+            System.out.println("This is not a leap year");
+        }
+        }
+}
