a283009a792a5bbfc94def3062c0eaa513c62c7b
manju2518 <m.p.manju2006@gmail.com>
2025-03-13 11:43:35 +0530

    Neural program

--- positive_negative/Demo.java
+++ positive_negative/Demo.java
@@ -2,10 +2,14 @@ public class Demo
 {
     public static void main(String args[])
     {
-        int num=-2;
+        int num=0;
         if(num>0)
         {
             System.out.println("It is a positive value");
+        }
+        else if(num==0)
+        {
+            System.out.println("The number is neutral");
         }
         else
         {
