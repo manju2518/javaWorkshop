5192bf24827097050e2286d7961796525343b3c8
manju2518 <m.p.manju2006@gmail.com>
2025-03-13 11:09:23 +0530

    Voting ability program

---
+++ voting_ability/Demo.java
@@ -0,0 +1,15 @@
+public class Demo
+{
+    public static void main(String args[])
+    {
+        int age=67;
+        if(age>=18)
+        {
+            System.out.println("Valid for voting");
+        }
+        else
+        {
+            System.out.println("Invalid for voting");
+        }
+    }
+}
