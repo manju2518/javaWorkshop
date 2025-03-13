582e451cef5a4c9f29f130a31517cce3d6238518
manju2518 <m.p.manju2006@gmail.com>
2025-03-13 14:11:53 +0530

    Even number problem

---
+++ even_number/Demo.java
@@ -0,0 +1,11 @@
+public class Demo
+{
+    public static void main(String args[])
+    {
+        for(int i=11;i<=20;i++)
+        {
+            if(i%2==0)
+            System.out.println(i);
+        }
+    }
+}
