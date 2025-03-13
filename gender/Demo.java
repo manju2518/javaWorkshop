58977655872f3f9c106371d7e36997bfd92b6be9
manju2518 <m.p.manju2006@gmail.com>
2025-03-13 11:32:57 +0530

    Gender program

---
+++ gender/Demo.java
@@ -0,0 +1,11 @@
+public class Demo{
+    public static void main(String args[]){
+        String gender="m";
+        if(gender=="m"){
+            System.out.println("Male");
+        }
+        else{
+            System.out.println("Female");
+        }
+    }
+}
