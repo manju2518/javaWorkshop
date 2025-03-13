2ae2fb1e95c1a9a78a1412f1ff1df4051dfc4096
manju2518 <m.p.manju2006@gmail.com>
2025-03-13 10:48:50 +0530

    Conditional statement program

---
+++ conditional_statement/Demo.java
@@ -0,0 +1,10 @@
+public class Demo{
+    public static void main(String args[]){
+        if(true){
+            System.out.println("It is true value");
+        }
+        if(false){
+            System.out.println("It is false value");
+        }
+    }
+}
