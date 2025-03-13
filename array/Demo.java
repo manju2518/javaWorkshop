e3bcbfd0d119868e5278f0264ec993dc881ff69e
manju2518 <m.p.manju2006@gmail.com>
2025-03-13 14:17:52 +0530

    Array program

--- array/Demo.java
+++ array/Demo.java
@@ -1,5 +1,6 @@
 public class Demo{
-    public static void main(String args[]){
+    public static void main(String args[])
+    {
         int num[]={10,20};
         System.out.println(num[0]);
     }
