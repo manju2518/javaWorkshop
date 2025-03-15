ee3da25781865217b4e4e69984116adc79a6e40b
kaviyavijayakumar331 <kaviya123friend@gmail.com>
2025-03-15 14:14:06 +0530

    Toll Fee program

---
+++ Traveling/tollfee.java
@@ -0,0 +1,40 @@
+import java.util.*;
+public class tollfee{
+    public static void main(String  args[])
+    {
+        
+        int car=100;
+        double car2=car*0.1;
+        double car3=car-car2;
+        int truck=200;
+        double truck2=truck*0.1;
+        double truck3=truck-truck2;
+    
+    
+        System.out.println("1.Car\n 2.truck\n 3.RFIDCar\n 4.RFID truck");
+        Scanner sc=new Scanner(System.in);
+        while(true)
+        {
+        System.out.println("Enter the Vechicle:");
+        int i=sc.nextInt();
+       
+        if(i==1)
+        {
+            System.out.println("Toll Fee:"+car);
+        }
+         else if(i==2)
+        {
+            System.out.println("Toll Fee:"+truck);
+        }
+        else if(i==3)
+        {
+            System.out.println("Toll Fee:"+car3);
+        }
+        else if(i==4)
+        {
+            System.out.println("Toll Fee:"+truck3);
+        }
+        }
+
+    }
+}
