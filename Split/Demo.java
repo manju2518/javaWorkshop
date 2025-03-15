d5026795a816f40017b1c9afab48d012980fdb24
kaviyavijayakumar331 <kaviya123friend@gmail.com>
2025-03-15 10:20:04 +0530

    Split program

---
+++ Split/Demo.java
@@ -0,0 +1,7 @@
+public class Demo{
+    public static void main(String args[]){
+        String name="Hello,World";
+        String names[]=name.split(",");
+        System.out.println(names[0]);
+    }
+}
