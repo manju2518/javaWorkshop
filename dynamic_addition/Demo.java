06dc0b25829bca898ecbeaf3200944b33bb62fd9
kaviyavijayakumar331 <kaviya123friend@gmail.com>
2025-03-15 11:26:04 +0530

    Dynamic addition program

---
+++ dynamic_addition/Demo.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Demo{
+    public static void main(String args[]){
+      Scanner sc=new Scanner(System.in);
+      System.out.println("Enter the value of x:");
+      int x=sc.nextInt();
+      System.out.println("enter the value of y:");
+      int y=sc.nextInt();
+      int z=x+y;
+      System.out.println("The addition of x and y:"+z);  
+    }
+}
