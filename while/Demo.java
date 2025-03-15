a09daeeb5b0f78961eb56dcea3935cddab1908b3
kaviyavijayakumar331 <kaviya123friend@gmail.com>
2025-03-15 11:03:48 +0530

    While program

---
+++ while/Demo.java
@@ -0,0 +1,10 @@
+public class Demo{
+    public static void main(String args[]){
+        int i=92;
+        while(i<=98)
+        {
+            System.out.println(i);
+            i=i+1;
+        }
+    }
+}
